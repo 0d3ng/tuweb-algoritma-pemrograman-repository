30a4a52dd447440bc275c75b13731d0ab488ea35
0d3ng <noprianto@polnema.ac.id>
2026-06-06 15:07:37 +0900

    add array function

--- src/App.java
+++ src/App.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Nilai argumen ke-" + i + " : " + args[i]);
+        }
+
         // deklarsi variabel
         float nilai_angka;// snake case
         String nilaiHuruf, keterangan;// camel case
+        String nilaiHurufRef[] = {"A", "B+", "B", "C", "D", "E"}; // array
         int nilai_uts, nilai_uas, nilai_tugas;
+        int nilai_mhs[] = new int[3]; // array
         Scanner input = new Scanner(System.in);
 
         // wellcome screen
@@ -15,39 +21,49 @@ public class App {
 
         // input nilai
         System.out.print("Masukkan nilai UTS    : ");
-        nilai_uts = input.nextInt();
+        nilai_mhs[0] = input.nextInt();
         System.out.print("Masukkan nilai UAS    : ");
-        nilai_uas = input.nextInt();
+        nilai_mhs[1] = input.nextInt();
         System.out.print("Masukkan nilai Tugas  : ");
-        nilai_tugas = input.nextInt();
+        nilai_mhs[2] = input.nextInt();
+
+
 
         // tampilkan input
-        // System.out.println("Nilai UTS    : " + nilai_uts);
-        // System.out.println("Nilai UAS    : " + nilai_uas);
-        // System.out.println("Nilai Tugas  : " + nilai_tugas);
+        // System.out.println("Nilai UTS    : " + nilai_mhs[0]);
+        // System.out.println("Nilai UAS    : " + nilai_mhs[1]);
+        // System.out.println("Nilai Tugas  : " + nilai_mhs[2]);]
+
+        // for(int nilai : nilai_mhs){
+        //     System.out.println("Nilai : " + nilai);
+        // }
+
+        // for(int i = 0; i < nilai_mhs.length; i++){
+        //     System.out.println("Nilai : " + nilai_mhs[i]);
+        // }
 
         // proses hitung nilai akhir
-        nilai_angka = (float) (0.30 * nilai_uts + 0.50 * nilai_uas + 0.20 * nilai_tugas);
+        nilai_angka = (float) (0.30 * nilai_mhs[0] + 0.50 * nilai_mhs[1] + 0.20 * nilai_mhs[2]);
         // System.out.println("Nilai Akhir  : " + nilai_angka);
 
         // proses hitung nilai huruf
         if (nilai_angka >= 80) {
-            nilaiHuruf = "A";
+            nilaiHuruf = nilaiHurufRef[0];
         } else if (nilai_angka >= 77) {
-            nilaiHuruf = "B+";
+            nilaiHuruf = nilaiHurufRef[1];
         } else if (nilai_angka >= 60) {
-            nilaiHuruf = "B";
+            nilaiHuruf = nilaiHurufRef[2];
         } else if (nilai_angka >= 50) {
-            nilaiHuruf = "C";
+            nilaiHuruf = nilaiHurufRef[3];
         } else if (nilai_angka >= 25) {
-            nilaiHuruf = "D";
+            nilaiHuruf = nilaiHurufRef[4];
         } else {
-            nilaiHuruf = "E";
+            nilaiHuruf = nilaiHurufRef[5];
         }
         // System.out.println("Nilai Huruf  : " + nilaiHuruf);
 
         // tambahkan keterangan
-        if (nilaiHuruf.equals("A") || nilaiHuruf.equals("B+") || nilaiHuruf.equals("B") || nilaiHuruf.equals("C")) {
+        if (nilaiHuruf.equals(nilaiHurufRef[0]) || nilaiHuruf.equals(nilaiHurufRef[1]) || nilaiHuruf.equals(nilaiHurufRef[2]) || nilaiHuruf.equals(nilaiHurufRef[3])) {
             keterangan = "Lulus";
         } else {
             keterangan = "Tidak Lulus";
@@ -59,6 +75,6 @@ public class App {
         System.out.println("====================================");
         System.out.println("   REKAPITULASI NILAI AKHIR");
         System.out.println("====================================");
-        System.out.printf("Nilai UTS    : %d%nNilai UAS    : %d%nNilai Tugas  : %d%nNilai Akhir  : %.2f%nNilai Huruf  : %s%nKeterangan   : %s%n", nilai_uts, nilai_uas, nilai_tugas, nilai_angka, nilaiHuruf, keterangan);
+        System.out.printf("Nilai UTS    : %d%nNilai UAS    : %d%nNilai Tugas  : %d%nNilai Akhir  : %.2f%nNilai Huruf  : %s%nKeterangan   : %s%n", nilai_mhs[0], nilai_mhs[1], nilai_mhs[2], nilai_angka, nilaiHuruf, keterangan);
     }
 }
